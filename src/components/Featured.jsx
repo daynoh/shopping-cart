037bb4ec05707346626c96dcf010075cbfe0a1cc
daynoh <justdaynoh@gmail.com>
2023-03-10 16:37:31 +0300

    Coding the backend work

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import planets from './Products.json'
 const Featured = () => {
+    const [isClicked,setIsClicked] = useState([false])
+    const [clickedProduct, setClickedProduct] = useState('')
+    const planetList = planets.planets.map(planet =>
+    <div className= 'products' onClick={()=>productClickhandler(planet)} key= {planet.name}>
+      <div className="name">{planet.name}</div>
+      <div>{planet.price}</div>
+      </div>)
+        
+    
+    const productClickhandler =(planet)=>
+    {
+      setIsClicked(true)
+      setClickedProduct(planet.name)
+    }
+    const something = planets.planets.map(planet =><h2 onClick={()=>productClickhandler(planet)}>{planet.name}</h2>)
   return (
     <div>
-      <h1>This is featured</h1>
+      {isClicked && <h1>This is the clicked product{clickedProduct}</h1>}
+      {console.log(planetList)}
+      {planetList}
     </div>
   )
 }
